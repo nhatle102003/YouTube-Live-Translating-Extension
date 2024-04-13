20044fcb2d6c63e5c29872c105c30eb67661df5c
nhatle102003 <nhle102003@gmail.com>
2024-04-13 11:58:04 -0500

    Clean up chat json template to make sure it more readable

--- scrape.js
+++ scrape.js
@@ -1,26 +1,34 @@
 
+let messageStack =  new Set();
 var port = chrome.runtime.connect({name: "content-script"});
 port.onMessage.addListener(function(message) {
+    
     if(message.request === "get-chat-messages"){
         setTimeout(() =>{
             const config = { attributes: true, childList: false, subtree: false};
             const targetNode = document.querySelector("#chatframe").contentWindow.document.querySelector("div#items.style-scope.yt-live-chat-item-list-renderer");
             console.log("this is target node", targetNode)
             const callback = (mutationList) => {
-                for(const mutation of mutationList){
+                if(mutationList[0].type === 'attributes'){
                     var repsonse = {
-                        author: mutation.target.lastChild.querySelector("span#author-name").innerText,
-                        message: mutation.target.lastChild.querySelector("span#message").innerText
+                        author: mutationList[0].target.lastChild.querySelector("span#author-name").innerText,
+                        message: mutationList[0].target.lastChild.querySelector("span#message").innerText,
+                        messageId: mutationList[0].target.lastChild.id
                     }
-                    console.log(mutation.target.lastChild.querySelector("span#message").innerText);
-                    console.log("mutation oberseved");
-                    port.postMessage({author: repsonse.author})
-                    port.postMessage({message: repsonse.message})
                     
+                    if(!messageStack.has(repsonse.messageId)){
+                        messageStack.add(repsonse.messageId);
+                        port.postMessage({chatMessage: {id: repsonse.messageId, author: repsonse.author, message: repsonse.message}});
+                        
+                    }
                 }
+
             }
             const observer = new MutationObserver(callback);
-            observer.observe(targetNode, config);
+            setTimeout(() => {
+                observer.observe(targetNode, config);
+            }, 1000)
+            
         }, 5000)
     }
 });
