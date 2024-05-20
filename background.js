ae1caaf4af0b58373ac7c4757d1c3cf769920abc
nhatle102003 <nhle102003@gmail.com>
2024-05-20 00:21:29 -0500

    Add a post route to local set up of libre translate instance

--- background.js
+++ background.js
@@ -1,6 +1,24 @@
 
+let seenMessage = new Set();
+
+const option = {
+    method: "POST",
+    headers: {
+            "Host": "http://localhost:8080/translate", 
+            'Content-Type': 'application/json',},
+    body: {},
+    mode : 'cors'
+};
+///////////////////////////////////////////////////////////
+
+function setMessagesToHeader(option, message){
+    option.body = JSON.stringify({"text" : [message], "target_lang": "EN" });
+}
+
+
+
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('SHIT!.');
+    console.log('The application is running!');
     console.log(chrome.tabs.TabStatus.COMPLETE === "complete")
 })
 
@@ -15,10 +33,46 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
             console.assert(port.name === "content-script");
             port.postMessage({request : "get-chat-messages"});
             port.onMessage.addListener(function(msg){
-                if(msg.author){
-                    console.log("I recieved ", msg.author);
-                }else if (msg.message){
-                    console.log("I received ", msg.message);
+                if(msg.chatMessage){
+                    if(!seenMessage.has(msg.chatMessage.id)){
+                        console.log(msg.chatMessage.id, msg.chatMessage.author, msg.chatMessage.message); // debug line
+                        seenMessage.add(msg.chatMessage.id);
+                        setMessagesToHeader(option, msg.chatMessage.message);
+                        console.log(option);
+                        
+                        setTimeout(() => {
+                            fetch("http://localhost:8080/libre-translate", option)
+                            .then((response) => {
+                                if (response.status === 403) {
+                                    throw new Error("Something went wrong on API server!");
+                                }
+                                return response.json();
+                            })
+                            .then((data) => {
+                                console.log(data);
+                                // Handle the translated response data here
+                            })
+                            .catch((error) => {
+                                console.error("Error translating:", error);
+                            });
+                        }, 1000)
+                        // setTimeout(() => {
+                        //     fetch("http://localhost:8080/translate", option)
+                        //     .then((response) => {
+                        //         if (response.status === 403) {
+                        //             throw new Error("Something went wrong on API server!");
+                        //         }
+                        //         return response.json();
+                        //     })
+                        //     .then((data) => {
+                        //         console.log(data);
+                        //         // Handle the translated response data here
+                        //     })
+                        //     .catch((error) => {
+                        //         console.error("Error translating:", error);
+                        //     });
+                        // }, 1000)
+                    }
                 }
             });
         });
